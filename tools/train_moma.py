96d44e44a76c5b1bf705e14f7a5ce18e824276c4
Zane Durante <zanedurante@gmail.com>
2022-02-28 22:37:03 +0000

    Cleaned training script

--- tools/train_moma.py
+++ tools/train_moma.py
@@ -34,67 +34,34 @@ from detectron2.data import (
     build_detection_train_loader,
 )
 from detectron2.engine import default_argument_parser, default_setup, default_writers, launch
-from detectron2.evaluation import (
-    CityscapesInstanceEvaluator,
-    CityscapesSemSegEvaluator,
-    COCOEvaluator,
-    COCOPanopticEvaluator,
-    DatasetEvaluators,
-    LVISEvaluator,
-    PascalVOCDetectionEvaluator,
-    SemSegEvaluator,
-    inference_on_dataset,
-    print_csv_format,
-)
+
 from detectron2.modeling import build_model
 from detectron2.solver import build_lr_scheduler, build_optimizer
 from detectron2.utils.events import EventStorage
 
+from detectron2.evaluation import COCOEvaluator
+
+
 import wandb
 
-from prepare_moma import register_moma
+from prepare_moma import register_dataset
 
 logger = logging.getLogger("detectron2")
 
+# Right now we are just using COCOEvaluator... (may want to modify, so we have this)
+class MOMAEvaluator(COCOEvaluator):
+    def __init__(self, *args, **kwards):
+        super().__init__(*args, **kwargs)
+        
+    
 
 def get_evaluator(cfg, dataset_name, output_folder=None):
-    """
-    Create evaluator(s) for a given dataset.
-    This uses the special metadata "evaluator_type" associated with each builtin dataset.
-    For your own dataset, you can simply create an evaluator manually in your
-    script and do not have to worry about the hacky if-else logic here.
-    """
+
     if output_folder is None:
         output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
-    evaluator_list = []
-    evaluator_type = MetadataCatalog.get(dataset_name).evaluator_type
-    if evaluator_type in ["sem_seg", "coco_panoptic_seg"]:
-        evaluator_list.append(
-            SemSegEvaluator(
-                dataset_name,
-                distributed=True,
-                output_dir=output_folder,
-            )
-        )
-    if evaluator_type in ["coco", "coco_panoptic_seg"]:
-        evaluator_list.append(COCOEvaluator(dataset_name, output_dir=output_folder))
-    if evaluator_type == "coco_panoptic_seg":
-        evaluator_list.append(COCOPanopticEvaluator(dataset_name, output_folder))
-    if evaluator_type == "cityscapes_instance":
-        return CityscapesInstanceEvaluator(dataset_name)
-    if evaluator_type == "cityscapes_sem_seg":
-        return CityscapesSemSegEvaluator(dataset_name)
-    if evaluator_type == "pascal_voc":
-        return PascalVOCDetectionEvaluator(dataset_name)
-    if evaluator_type == "lvis":
-        return LVISEvaluator(dataset_name, cfg, True, output_folder)
-    if len(evaluator_list) == 0:
-        raise NotImplementedError(
-            "no Evaluator for the dataset {} with the type {}".format(dataset_name, evaluator_type)
-        )
-    if len(evaluator_list) == 1:
-        return evaluator_list[0]
-    return DatasetEvaluators(evaluator_list)
+    
+    # Get MOMA dataset evaluator   
+    return MOMAEvaluator(dataset_name, output_dir=output_folder)
 
 
 def do_test(cfg, model):
@@ -193,7 +160,7 @@ def main(args):
     #wandb.init(project="MOMA-obj-detect", entity="durante")
     
     # Register MOMA dataset
-    register_moma()
+    register_dataset()
     
     cfg = setup(args)
     print("cfg:", cfg)
